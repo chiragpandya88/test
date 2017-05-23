82b99bb45aabc22f44daf3c0576851e0f44f3410
chiragpandya88 <chiragpandya88@gmail.com>
2017-05-23 11:27:52 +0530

    Experimenting with R v1

--- testScript.R
+++ testScript.R
@@ -1,2 +1,13 @@
-a<-c(1,2,3,4,5)
-print(mean(a))
+# convert to local data frame
+flights <- tbl_df(hflights)
+
+
+filter(flights, Month == 1, DayofMonth == 1)
+
+filter(flights,UniqueCarrier %in% c("AA","UA"))
+
+select(flights, DepTime, ArrTime, FlightNum)
+
+flights %>% select(UniqueCarrier,DepDelay) %>% filter(DepDelay > 60)
+
+flights %>%  select(UniqueCarrier, DepDelay) %>%  arrange(DepDelay)
